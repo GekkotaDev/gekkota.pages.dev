118cd1d1d47070a1176fc26eadcf6313e1d8e37d
GekkotaDev <114091010+GekkotaDev@users.noreply.github.com>
2026-03-11 22:50:37 +0800

    fix: rss data

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,11 @@
 import type { APIRoute } from "astro";
 
 import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
-export const GET = ((context) => {
+export const GET = (async (context) => {
+	const blog = await getCollection("posts");
+
 	return rss({
 		// `<title>` field in output xml
 		title: "gekkotadev's blog",
@@ -14,7 +17,14 @@ export const GET = ((context) => {
 		site: context.site!,
 		// Array of `<item>`s in output xml
 		// See "Generating items" section for examples using content collections and glob imports
-		items: [],
+		items: blog.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.dates.published,
+			description: post.data.description,
+			// Compute RSS link from post `id`
+			// This example assumes all posts are rendered as `/blog/[id]` routes
+			link: `/posts/${post.id}/`,
+		})),
 		// (optional) inject custom xml
 		customData: `<language>en-us</language>`,
 	});
